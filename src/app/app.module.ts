d9e10315f419ea8b6cb2308e13bdbb2f573359e1
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-05-18 10:08:18 +0200

    boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule, Routes} from "@angular/router";
 
+const approutes: Routes = [
+  /*{path: 'exemple', component: ExempleComponent},
+  {path: 'del/:id', component: ExempleDelComponent},
+  {path: '', component: ExempleComponent },
+  {path: '**', component: ExempleComponent },*/
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(approutes),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
